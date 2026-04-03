917ffc13ee178bb03f86f8bfb8f5e1f98de676d0
Juan Rodriguez <juanaviator1@gmail.com>
2026-04-03 11:47:48 -0500

    f

--- vite.config.ts
+++ vite.config.ts
@@ -16,12 +16,12 @@ export default defineConfig({
         theme_color: "#f59e0b",
         icons: [
           {
-            src: "/logo.svg",
+            src: "/logo2.svg",
             sizes: "192x192",
             type: "image/svg+xml",
           },
           {
-            src: "/logo.svg",
+            src: "/logo2.svg",
             sizes: "512x512",
             type: "image/svg+xml",
           },
