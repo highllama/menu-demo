7b5f188305eac7922d729d7a02bba0119be42cbd
Juan Rodriguez <juanaviator1@gmail.com>
2026-04-08 09:56:35 -0500

    aaa

--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // VitePWA({
-    //   registerType: "autoUpdate",
-    //   manifest: {
-    //     name: "Kalendu Menu",
-    //     short_name: "Kalendu Menu",
-    //     description: "Kalendu Menu",
-    //     theme_color: "#ff7a00",
-    //     background_color: "#ff7a00",
-    //     icons: [
-    //       // {
-    //       //   src: "/logo2.svg",
-    //       //   sizes: "192x192",
-    //       //   type: "image/svg+xml",
-    //       // },
-    //       {
-    //         src: "/512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //         purpose: "any",
-    //       },
-    //       {
-    //         src: "/512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //         purpose: "maskable",
-    //       },
-    //       // {
-    //       //   src: "/logo2.svg",
-    //       //   sizes: "512x512",
-    //       //   type: "image/svg+xml",
-    //       // },
-    //       // {
-    //       //   src: "/448x448.png",
-    //       //   sizes: "448x448",
-    //       //   type: "image/png",
-    //       // },
-    //     ],
-    //   },
-    // }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
